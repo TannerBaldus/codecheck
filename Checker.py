49da79f1a241d66abdb9975d1c4850e1d29ea426
TannerBaldus <tanner@irontower.(none)>
2013-09-12 00:41:27 -0700

    Created IO class with functions: getInput, getExpected,lineReader,getCases
    Added compare funtion to Checker class
    need to rework documentation of checker class

--- Checker.py
+++ Checker.py
@@ -1,4 +1,77 @@
 import subprocess as sub
+class IO:
+
+	def lineReader(self, Input, type):
+
+		if type == "string":
+			output = ""
+			for line in Input.readlines():
+				output += (" " + line)
+			return output 
+
+		elif type == "int":
+			for line in Input.readlines():
+				output = int(line)
+			return output
+
+		elif type == "list":
+			output = []
+			for line in Input.readlines():
+				output.append(str(line))
+			return output
+
+
+	def getCases(self, ProblemNumber):
+		problem = "problems/"+str(ProblemNumber)
+		casesFile = open(problem+"/cases.txt","r")
+		cases = self.lineReader(CasesFile,"int")
+		return cases
+
+	def getExpected(ProblemNumber,CaseNumber):
+		problem = "problems/"+str(ProblemNumber)
+		case = "/case"+str(CaseNumber)
+		expectedFile = open(problem+"/"+case+"/output.txt",'r')
+		expected = self.lineReader(expectedFile,"list")
+		return expected
+
+	def getInput(ProblemNumber,CaseNumber):
+		problem = "problems/"+str(ProblemNumber)
+		case = "/case"+str(CaseNumber)
+		inputFile = open(problem+"/"+case+"/input.txt",'r')
+		Input = self.lineReader(inputFile,"list")
+		return Input
+
+
+	def OutputHandler(self, TestInput, StudentCode,ExpectedOut,linelimit, message):
+		"""
+
+		"""
+
+		FullInput = "uliimit -t 2; python" + TestInput  
+		proc =  sub.Popen( FullInput, shell = True, stdout = sub.PIPE, stderr = sub.PIPE)
+		StudentOut = []
+
+		out = proc.stdout
+		error = proc.stderr
+
+		i = 0
+		for line in out.readlines():
+			StudentOut.append(line)
+			i += 1
+			if i > linelimit:
+				break
+				return "loop"
+
+		if error != None:
+			for line in error.readlines():
+				message.append(str(line)
+			return "error"
+		
+		else:
+			return StudentOut 
+
+
+
 class Checker(object):
 	"""
 	Summary
@@ -25,24 +98,15 @@ class Checker(object):
 		self.StudentId = StudentId
 		self.problem = problem
 		self.message = ""
-		self.CorrectFlag = True
+		self.CorrectFlag = 
 
-	def OutputHandler(self, TestInput,StudentCode,ExpectedOut):
-		"""
 
-		"""
-		FullInput = TestInput.extend(StudentCode)
-		RawOut =  sub.Popen( FullInput, stdout = sub.PIPE, stderr = sub.PIPE)
-		StudentOut = []
-
-		if RawOut.stderr:
-			for line in RawOut.stderr.readlines():
-				self.message.append(line)
-			return "error"
-
-		else:
-			for line in RawOut.readlines():
-				StudentOut.append(line)
-
-			return "okay" 
-
+	def compare(expected, given):
+		message = self.message
+		i = 0
+		for line in expected:
+			if line != given[i]:
+				message.append("Expected: "+str(line))
+				message.append("Given:    "+str(given[i]))
+				i += 1
+	def check():
