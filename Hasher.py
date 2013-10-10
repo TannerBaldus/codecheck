5156cf6a423d6ba7a5897215f96181f8ef416f01
Tanner Baldus <tanner@irontop.(none)>
2013-10-09 21:21:19 -0700

    created Hasher module to generate token

---
+++ Hasher.py
@@ -0,0 +1,24 @@
+import hashlib
+class Token(object):
+	def __init__(self,studentId,correctFlag):
+		self.studentId = studentId
+		self.correctFlag = correctFlag
+		self.salt = ""
+
+	def generateToken(self):
+		message = []
+		token = hashlib.md5(self.studentId+self.salt)
+		if self.correctFlag == True:
+			token.upadate("correct")
+			message.append("Congrats you got it right! Here's your token: ")
+			message.append(token.hexdigest())
+		else:
+			token.upadate("wrong")
+			message.append("Sorry your code didn't pass all of the test cases see below for what went wrong.")
+			message.append("You still get a token for trying: ")
+			message.append(token.hexdigest())
+	
+		return message
+
+
+
